06427d65464c6d831b92ca3e7c0d8c71bba5ef22
sarabdev <sarab@contrivers.dev>
2023-10-12 14:40:24 +0500

    added script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import { Toaster } from 'react-hot-toast';
 import { QueryClient, QueryClientProvider } from 'react-query';
-
+import Script from 'next/script'
 import { appWithTranslation } from 'next-i18next';
 import type { AppProps } from 'next/app';
 import { Raleway } from 'next/font/google';
@@ -15,6 +15,7 @@ function App({ Component, pageProps }: AppProps<{}>) {
   const queryClient = new QueryClient();
 
   return (
+    <>
     <ClerkProvider>
     <AuthContextProvider>
     <div className={inter.className}>
@@ -25,6 +26,9 @@ function App({ Component, pageProps }: AppProps<{}>) {
     </div>
     </AuthContextProvider>
     </ClerkProvider>
+    <Script src="//code.tidio.co/shqay9dyzehoqprjvcmzj0veozd4gfy8.js" />
+
+    </>
   );
 }
 
